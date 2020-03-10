bd06288bd252e88d76eb50f183434ce5c54ce744
William Chen <williamchen@Williams-MacBook-Air.local>
2020-03-10 10:56:22 -0400

    added the sql info and added controller files

---
+++ server/controller/storeController.js
@@ -0,0 +1,4 @@
+const storeController = {}
+
+
+module.exports = storeController;
---
+++ server/models/storeModels.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg')
+
+// Connection string
+const URI = 'postgres://ubynxewf:WGXXZnrHXUp8DoIU8Jj5naXOMvmu56Ok@drona.db.elephantsql.com:5432/ubynxewf'
+
+// Connection 
+const pool = new Pool({ connectionString: URI });
+
+// Create the store table;
+const createStore = 'CREATE TABLE IF NOT EXISTS store (id SERIAL PRIMARY KEY, item VARCHAR, price MONEY, category VARCHAR, era INT, stock INT, link VARCHAR, description VARCHAR)'
+// Generate the table if the table doesn't exist
+pool.query(createStore, (err) => {
+  if (err) {
+    console.log("Error in create table!")
+  }
+})
+
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log("Executed query: ", text);
+    return Pool.query(text, params, callback);
+  }
+}
