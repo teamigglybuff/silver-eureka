902a3367da17265fdd2e540590b4fe7fc98e9d2b
Eli <eligallipoli317@gmail.com>
2020-03-10 12:23:25 -0400

    added cart action creators

--- client/actions/cart-actions.js
+++ client/actions/cart-actions.js
@@ -1,2 +1,11 @@
 import * as types from '../constants/action-types';
 
+export const addItem = (item) => ({
+  type: types.ADD_TO_CART,
+  item,
+});
+
+export const deleteItem = (item) => ({
+  type: types.DELETE_FROM_CART,
+  item,
+});
